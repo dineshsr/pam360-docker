49104da8178e93bde6a60109f6ac5f9981ad4cfd
dineshsr <dineshsr@zohomail.com>
2020-12-31 18:25:09 +0530

    [UPDATE]: Re-writing Dockerfile with user based installation.

---
+++ pam360/entrypoint.sh
@@ -0,0 +1,103 @@
+#!/usr/bin/env bash
+
+POSTGRES_HOST=${POSTGRES_HOST:-database}
+POSTGRES_PORT=${POSTGRES_PORT:-5432}
+POSTGRES_DB=${POSTGRES_DB:-pmp}
+POSTGRES_USER=${POSTGRES_USER:-pmp}
+POSTGRES_PASSWORD=${POSTGRES_PASSWORD:-pmppassword}
+
+SERVER_STATE=${SERVER_STATE:-master}
+TIMEOUT_DB=${TIMEOUT_DB:-120}
+TIMEOUT_PAM=${TIMEOUT_PAM:-600}
+
+PAM_PORT=${PAM_PORT:-8282}
+
+sync_pmp_home_dir() {
+  local PAM_TMP_HOME="/srv/PAM"
+  local lockfile="${PAM_HOME}/.INIT_SYNC_DONE"
+
+  if ! [[ -e "$lockfile" ]]
+  then
+    echo "Copying PAM_HOME contents to $PAM_HOME"
+    if cp -a "$PAM_TMP_HOME" "$PAM_HOME"
+    then
+      touch "$lockfile"
+    fi
+  fi
+}
+
+set_server_state() {
+  echo "$SERVER_STATE" > "${PAM_HOME}/conf/serverstate.conf"
+}
+
+init_conf_dir() {
+  local ext_conf_path=/config
+  local lockfile="${ext_conf_path}/.INIT_SYNC_DONE"
+
+  if ! [[ -e "$lockfile" ]]
+  then
+    echo "Copying config files to ${ext_conf_path}."
+    if cp -a "${PAM_HOME}/conf.orig/"* "$ext_conf_path"
+    then
+      touch "$lockfile"
+    fi
+  fi
+
+  # Ensure PAM_HOME/conf is symlinked to /config
+  if ! [[ -L "${PAM_HOME}/conf" ]]
+  then
+    ln -sf "$ext_conf_path" "${PAM_HOME}/conf"
+  fi
+
+  set_server_state
+}
+
+wait_for_db() {
+  if ! wait-for-it.sh -t "$TIMEOUT_DB" "${POSTGRES_HOST}:${POSTGRES_PORT}"
+  then
+    echo -n "Timed out while trying to reach the database " >&2
+    echo "${POSTGRES_HOST}:${POSTGRES_PORT} - Timeout: ${TIMEOUT_DB}s" >&2
+    exit 8
+  fi
+}
+
+start_pam() {
+  /etc/init.d/pam360-service start
+
+  (tail -f "${PAM_HOME}"PAM/logs/*)&
+}
+
+wait_for_pam() {
+  if ! wait-for-it.sh -t "$TIMEOUT_PAM" "localhost:${PAM_PORT}"
+  then
+    echo "PAM360 failed to start - Timeout: ${TIMEOUT_PAM}s" >&2
+    exit 7
+  fi
+}
+
+pam_is_running() {
+  # while ps -aux | grep -q "${PAM_HOME}"
+  /etc/init.d/pam360-service status | grep -q "PAM360 is running"
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
+then
+  sync_pmp_home_dir
+
+  if [[ -n "$PAM_UPGRADE" ]]
+  then
+    echo "!!! Started in upgrade mode." >&2
+    echo "The PAM service has *NOT* been started." >&2
+    echo "To disable please unset PAM_UPGRADE." >&2
+    sleep infinity
+  else
+    # TODO Start the database
+    start_pam
+    wait_for_pam
+
+    while pam_is_running
+    do
+      sleep 5
+    done
+  fi
+fi
---
+++ pam360/install.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+set -euxo
+
+PAM_VERSION="$1"
+PAM_TMP_HOME="/srv/PAM"
+PAM_INSTALLER="/tmp/pam_installer.bin"
+
+install_dependencies() {
+  apt-get update
+  apt-get install -y curl unzip
+
+  curl -fsSL https://raw.githubusercontent.com/vishnubob/wait-for-it/master/wait-for-it.sh \
+    -o /usr/bin/wait-for-it.sh
+
+  chmod +x /usr/bin/wait-for-it.sh
+}
+
+cleanup() {
+  rm -rf \
+    /var/lib/apt/lists/* \
+    "${PAM_INSTALLER}" \
+    /tmp/pmp.properties
+}
+
+install_pam() {
+  if [[ -e "${PAM_INSTALLER}" ]]
+  then
+    echo "Using the local install binary at ${PAM_INSTALLER}"
+  else
+    install_bin=ManageEngine_PAM360_64bit.bin
+
+    url="https://archives.manageengine.com/privileged-access-management/${PAM_VERSION}/${install_bin}"
+    echo "Downloading PAM360 installer from $url"
+    curl -fsSL -o "${PAM_INSTALLER}" "$url"
+  fi
+
+  chmod +x "${PAM_INSTALLER}"
+
+  mkdir -p "$(dirname "$PAM_HOME")"
+  # Update PAM_HOME in properties (/srv/pmp was used as install path at the
+  # time of creation of the .properties file)
+  sed -i "s|/srv/pam|${PAM_HOME}|" /tmp/pmp.properties
+  "${PAM_INSTALLER}" -i silent -f /tmp/pmp.properties
+  fix_pmp_home
+
+  cd "${PAM_HOME}"
+
+  cd "bin"  # yup. That's required by pmp.sh 🤦
+  bash "${PAM_HOME}/bin/pam360.sh" install | grep "installed successfully"
+}
+
+fix_pmp_home() {
+  # If PAM_HOME does not end with a PMP dir then it get installed
+  # in PAM_HOME/PMP
+  if [[ "$(basename "$PAM_HOME")" != "PAM360" ]]
+  then
+    mv "${PAM_HOME}/PAM360" "/tmp/pmp.tmp"
+    mv "${PAM_HOME}" "/tmp/pmp_deleteme"
+    rmdir "/tmp/pmp_deleteme"
+    mv "/tmp/pmp.tmp" "${PAM_HOME}"
+  fi
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
+then
+  echo "Building container for PMP ${PAM_VERSION}"
+  install_dependencies
+  install_pam
+  cleanup
+
+  # Move PAM_HOME to PAM_HOME.orig (will be copied over to /data at runtime)
+  mv "$PAM_HOME" "$PAM_TMP_HOME"
+fi
